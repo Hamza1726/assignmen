374c07b0ef9ccd9cc4abf64cd1016704ff8621c3
Hamza1726 <96973102+Hamza1726@users.noreply.github.com>
2022-01-02 13:31:23 +0500

    Add files via upload

---
+++ Assignment phython.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[8]:
+
+
+print("Twinklke,twinkle,little star,")
+
+print("          How l wonder what you are!")
+print("                     Up above world so high,")
+print("                     Like a diamond in the sky.")
+print("Twinkle,twinkle,little star,")
+print("         How l wonder what you are")
+
+
+# In[11]:
+
+
+import sys 
+print("phython version")
+print(sys.version)
+print("phython info")
+print(sys.version_info)
+
+
+# In[1]:
+
+
+f=input("first name")
+l=input("last name")
+print(l +" "+ f)
+
+
+# In[9]:
+
+
+num1= int(input("Enter number 1 "))
+num2= int(input("Enter number 2 "))
+total= num1 + num2 
+print (total)
+          
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
